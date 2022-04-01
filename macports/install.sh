c42efe8b1b634a7596dc4aec06b6871eaa7e424f
Frederik Vos <frederik.vos@linvirt.nl>
2022-04-01 15:32:47 +0200

    initial release

---
+++ macports/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+port install vim +python310 +lua
+port select --set python python310
+port select --set python3 python310
+port install py310-pip
+port select --set pip pip310
+port select --set pip3 pip310
+port install kubectl-1.23
+port select --set kubectl kubectl1.23
+port install tmux
+port install tmux-pasteboard
+
