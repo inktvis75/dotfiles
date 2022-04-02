f0616d48ae2eb7670479b2ab503dd997ac506794
Frederik Vos <frederik.vos@linvirt.nl>
2022-04-02 18:34:39 +0200

    tmux

--- setup/setup.sh
+++ setup/setup.sh
@@ -7,3 +7,6 @@ fi
 
 # vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# tmux 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
