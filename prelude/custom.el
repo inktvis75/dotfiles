aca83d84fbe50b3ce616f6b21d43efb35856ec80
Frederik Vos <frederik.vos@linvirt.nl>
2024-04-24 21:54:49 +0200

    emacs config - lilypond

--- prelude/custom.el
+++ prelude/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-babel-lilypond-commands '("/opt/homebrew/bin/lilypond" "open" "open"))
  '(package-selected-packages
    '(treemacs-projectile treemacs-evil ox-reveal treemacs dashboard yaml-mode web-mode company-anaconda anaconda-mode lsp-ui lsp-mode company-auctex cdlatex auctex json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company consult orderless vertico exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window htmlize))
  '(warning-suppress-log-types '((comp) (comp) (comp) (comp) (comp) (comp)))
---
+++ prelude/lilypond.el
@@ -0,0 +1,6 @@
+;; Lilypond
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (org . t)
+   (lilypond . t)))
