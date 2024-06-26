f06cb07894b3abf774626b34030b0f7a87d8fa52
Frederik Vos <frederik.vos@linvirt.nl>
2024-06-26 10:14:52 +0200

    vterm support

--- emacs/shell.el
+++ emacs/shell.el
@@ -10,3 +10,16 @@
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize)
 )
+
+; Terminal
+(use-package vterm
+  :straight t
+  :custom (vterm-install t)
+  :config
+  (setq vterm-always-compile-module t)
+  (setq vterm-buffer-name-string "vterm %s")
+  (add-hook 'vterm-mode-hook
+            (lambda () (global-hl-line-mode 0)))
+  (setq vterm-max-scrollback 10000)
+)
+
