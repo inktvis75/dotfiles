5f781f305e0ca9c80d8254c3d13641dd5015d204
Frederik Vos <frederik.vos@linvirt.nl>
2024-07-25 16:13:02 +0200

    fix hugo on emacs

--- emacs/hugo.el
+++ emacs/hugo.el
@@ -5,4 +5,12 @@
 )
 (use-package easy-hugo
   :straight t
+  :config
+  (evil-make-overriding-map easy-hugo-mode-map 'normal)
+  :init
+   (setq easy-hugo-server-flags "-D") 
+   (setq easy-hugo-url "https://www.linvirt.nl")
+   (setq easy-hugo-basedir "~/Websites/linvirt.nl/")
+   (setq easy-hugo-postdir "content/blog")
+   (setq easy-hug-default-ext ".org")
 )
